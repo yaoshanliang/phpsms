e9645aef9ead24b3c009ee63c16c9db242251bd6
yaoshanliang <1329517386@qq.com>
2016-02-21 21:20:18 +0800

    add send api

--- src/agents/ChuanglanAgent.php
+++ src/agents/ChuanglanAgent.php
@@ -1,5 +1,6 @@
 <?php
 namespace Yaoshanliang\PhpSms;
+use Toplan\PhpSms\Agent;
 
 class ChuanglanAgent extends Agent {
     //override
@@ -9,24 +10,40 @@ class ChuanglanAgent extends Agent {
        //在这个方法中调用二级入口
        //根据你使用的服务商的接口选择调用哪个方式发送短信
        $this->sendContentSms($to, $content);
-       $this->sendTemplateSms($tempId, $to, $tempData);
+       // $this->sendTemplateSms($tempId, $to, $tempData);
     }
 
     //override
     //发送短信二级入口：发送内容短信
     public function sendContentSms($to, $content)
     {
-        //获取配置文件中的参数
-        $key = $this->apikey;
+        $url = 'http://222.73.117.158/msg/HttpBatchSendSM';
+
+        //创蓝接口参数
+        $params = array (
+            'account' => $this->apiAccount,
+            'pswd' => $this->apiPassword,
+            'msg' => '您好，您的验证码是：' . $content,
+            'mobile' => $to,
+            'needstatus' => true,
+            'product' => '',
+            'extno' => ''
+        );
+        $isPost = true;
 
         //可用方法:
-        Agent::sockPost($url, $query);//fsockopen
-        Agent::curl($url, array $params, bool $isPost);//curl
+        // Agent::sockPost($url, $query);//fsockopen
+        $result = Agent::curl($url, (array)$params, (bool)$isPost);//curl
+        $result=preg_split("/[,\r\n]/", $result['response']);
 
         //切记更新发送结果
-        $this->result('success', true);//是否发送成功
-        $this->result('info', $msg);//发送结果信息说明
-        $this->result('code', $code);//发送结果代码
+        if (0 == $result[1]) {
+            $this->result('success', true);//是否发送成功
+        } else {
+            $this->result('success', false);//是否发送成功
+        }
+        $this->result('info', '');//发送结果信息说明
+        $this->result('code', $result[1]);//发送结果代码
     }
 
     //override
